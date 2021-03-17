4f7c0c264de38391d54e3f6629532924305f24e1
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2021-03-17 16:21:18 -0300

    criations sendMailController

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysConller } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysConller();
 
+const sendMailController = new SendMailController();
+
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute)
 export {router}
