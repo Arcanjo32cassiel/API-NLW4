459249440dd86ecc8c39a145ca591c565b186bd3
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2021-03-18 15:37:46 -0300

    mapping of user and suvey tables

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -4,11 +4,11 @@ import { UsersRepository } from '../repositories/UsersRepository';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import SendMailService from '../services/SendMailService';
-import {resolve} from 'path';
+import { resolve } from 'path';
 
 class SendMailController {
     async execute(request: Request, response: Response) {
-        const {email, survey_id} = request.body;
+        const { email, survey_id } = request.body;
 
         const usersRepository = getCustomRepository(UsersRepository);
 
@@ -16,36 +16,49 @@ class SendMailController {
 
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-        const user = await usersRepository.findOne({email});
+        const user = await usersRepository.findOne({ email });
 
-        if(!user){
+        if (!user) {
             return response.status(400).json({
-            error: "User does not exists",
+                error: "User does not exists",
             });
         }
-        const survey = await surveysrepository.findOne({id: survey_id});
-        if(!survey){
+        const survey = await surveysrepository.findOne({ id: survey_id });
+        if (!survey) {
             return response.status(400).json({
-                error : "Survey does not  exists"
+                error: "Survey does not  exists"
             })
+        }
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            descripition: survey.descripition,
+            user_id: user.id,
+            link: process.env.URL_MAIL,
+        };
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{ user_id: user.id }, { value: null }],
+            relations: ["user", "survey"],
+        })
+        if (surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return response.json(surveyUserAlreadyExists);
         }
         // Save the informations at table surveyUser
         const surveyUser = surveysUsersRepository.create({
-            user_id:  user.id,
+            user_id: user.id,
             survey_id
         })
-        await   surveysUsersRepository.save(surveyUser);
+        await surveysUsersRepository.save(surveyUser);
         // Send e-mail to the user
-        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
-        const variables  ={
-            name:user.name,
-            title:survey.title,
-            descripition: survey.descripition 
-        }
-        await SendMailService.execute(email, survey.title, variables, npsPath)
         
+
+
+        await SendMailService.execute(email, survey.title, variables, npsPath)
+
         return response.json(surveyUser);
     }
 }
-export {SendMailController}
+export { SendMailController }
--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -1,28 +1,39 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
-import {v4 as uuid} from 'uuid';
+import { Entity, PrimaryColumn, Column, CreateDateColumn, ManyToMany, ManyToOne, JoinColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+import { User } from "./user";
+import { Survey } from "./Survey";
 
 
 @Entity("surveys_users")
-class SurveyUser{
+class SurveyUser {
     @PrimaryColumn()
     readonly id: string;
- 
-     @Column()
-     user_id: string;
- 
-     @Column()
-     survey_id: string;
 
-     @Column()
-     value: string;
+    @Column()
+    user_id: string;
 
-     @CreateDateColumn()
-     created_at: Date;
-     
-     constructor(){
-         if(!this.id){
-             this.id = uuid()
-         }
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "user_id" })
+    user: User
+
+    @Column()
+    survey_id: string;
+
+    @ManyToOne(() => Survey)
+    @JoinColumn({ name: "survey_id" })
+    survey: Survey
+
+
+    @Column()
+    value: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid()
+        }
+    }
 }
-}
-export {SurveyUser}
+export { SurveyUser }
