8178571e9c752080f6c6694cdcdca7cd0c91ac55
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2021-03-21 08:30:43 -0300

    middleware creation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 import * as yup from 'yup'
+import { AppError } from '../errors/AppError';
 
 class UserController{
     async create(request:Request, response:Response){
@@ -17,7 +18,7 @@ class UserController{
  try {
      await schema.validate(request.body, {abortEarly: false})
  }catch(err){
-    return response.status(400).json({error:err})
+    throw new AppError(err);
  }
         const useraRepository = getCustomRepository(UsersRepository);
 
@@ -26,10 +27,7 @@ class UserController{
             email
         })
         if(userAlreadyExists){
-            
-           return response.status(400).json({
-            error:"User already exists!",
-           })
+            throw new AppError("Survey does not  exists");
         }
         const user =   useraRepository.create({
             name, email
